e2b797bd006419222e15fccb3129f88647e7d686
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-09-05 11:33:25 +0330

    cofigure store

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// * Create database connection
+
+mongoose.connect("mongodb+srv://Ramin:.kbpZGf9J!r3Hbz@cluster0.p9ajz.mongodb.net/")
+    .then(() => console.log("MongoDB connected successfully 😎"))
+    .catch(error => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173/",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Authorization', 'Content-Type', 'Cache-Control', 'Expires', 'Pragma'],
+        credentials: true
+    })
+);
+
+app.use(cookieParser);
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
