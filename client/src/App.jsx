7861f9e101b8b463ebca8fc62dafc061fa5937d2
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2024-09-05 13:07:43 +0330

    auth check component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,16 @@ import Orders from "./pages/admin/Orders"
 import Features from "./pages/admin/Features"
 import ShopLayout from "./components/shop/ShopLayout"
 import NotFound from "./store/error/NotFound"
-import Checkout from "./pages/shop/Checkout"
-import Account from "./pages/shop/Account"
+import Checkout from "./pages/cart/Checkout"
+import Account from "./pages/account/Account"
 import List from "./pages/shop/List"
-import Home from "./pages/shop/Home"
+import Home from "./pages/home/Home"
+import CheckAuth from "./components/common/CheckAuth"
 
 
 function App() {
+
+
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
@@ -23,18 +26,26 @@ function App() {
       <h1>Header component</h1>
 
       <Routes>
-        <Route path="/auth" element={<AuthLayout />} >
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={true} user={{ name: "Ramin", role: "user" }}>
+            <AuthLayout />
+          </CheckAuth>
+        } >
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={true} user={{ name: "Ramin", role: "admin" }}>
+            <AdminLayout />
+          </CheckAuth>
+        } >
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="products" element={<Products />} />
           <Route path="orders" element={<Orders />} />
           <Route path="features" element={<Features />} />
         </Route>
         <Route path="/shop" element={<ShopLayout />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/list" element={<List />} />
         <Route path="/account" element={<Account />} />
         <Route path="/checkout" element={<Checkout />} />
